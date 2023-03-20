80c4f668487d542548423a007924209824b22c4f
LahiruQualitapps <lahiru.kahawatta@qualitapps.com>
2023-03-20 11:36:45 +0530

    commit by Lahiru

--- src/install.js
+++ src/install.js
@@ -1,7 +1,7 @@
 import HelloWorld from "./components/HelloWorld.vue";
 
 const HelloWorldSimple = {
-    install(Vue, options) {
+    install(Vue) {
         // Let's register our component globally
         // https://vuejs.org/v2/guide/components-registration.html
         Vue.component("hello-world", HelloWorld);
